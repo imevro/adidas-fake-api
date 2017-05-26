6b1ab14ba98148106a6375f01bdd909d53c31921
Evgeny Rodionov <git@erodionov.ru>
2017-05-26 13:59:41 +0300

    feat: add pagination

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 const express = require('express'); // eslint-disable-line import/newline-after-import
-const data = {
+const collections = {
   basketball: {
     shoes: require('./data/basketball-shoes.json'),
     apparel: require('./data/basketball-apparel.json'),
@@ -19,22 +19,29 @@ const data = {
 };
 
 const app = express();
+const router = express.Router();
 
-app.get('/products/:group/:type', (req, res) => {
+router.get('/products/:group/:type', (req, res) => {
   const { group, type } = req.params;
+  const { limit = 32, offset = 0 } = req.query;
 
-  if (data[group] && data[group][type]) {
-    res.send(data[group][type]);
+  if (collections[group] && collections[group][type]) {
+    const items = collections[group][type];
+
+    res.send({
+      items: [...items].splice(offset, limit),
+      total: items.length,
+    });
   } else {
     res.sendStatus(404);
   }
 });
 
-app.get('/products/:group/:type/:id', (req, res) => {
+router.get('/products/:group/:type/:id', (req, res) => {
   const { group, type, id } = req.params;
 
-  if (data[group] && data[group][type]) {
-    const product = data[group][type].find(p => p.id === id);
+  if (collections[group] && collections[group][type]) {
+    const product = collections[group][type].find(p => p.id === id);
 
     if (product) res.send(product);
     else res.sendStatus(404);
@@ -43,6 +50,7 @@ app.get('/products/:group/:type/:id', (req, res) => {
   }
 });
 
+app.use('/v1', router);
 app.listen(3000, () => {
   console.log('adidas-api listening on port 3000!'); // eslint-disable-line no-console
 });
